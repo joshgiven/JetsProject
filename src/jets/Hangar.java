f92c568a57c6da4d328618ea13ad0a367c4cc733
Josh Given <josh.given@gmail.com>
2016-12-15 17:21:46 -0700

    refactor Hanger to use List<>

--- src/jets/Hangar.java
+++ src/jets/Hangar.java
@@ -1,51 +1,28 @@
 package jets;
 
-public class Hangar {
-	private Jet[] jets;
-	private int numJets;
+import java.util.*;
 
+public class Hangar {
+	private List<Jet> jets;
+	
 	public Hangar() {
-		// chose 3 so that we exercise expandArray()
-		jets = new Jet[3];
-		numJets = 0;
+		jets = new ArrayList<>();
 	}
 	
 	public void addJet(Jet jet) {
 		if(jet == null)
 			return;
 		
-		if(numJets == jets.length) 
-			expandArray();
-		
 		if(!containsJet(jet)) {
-			jets[numJets] = jet;
-			numJets++;
+			jets.add(jet);
 		}
 	}
 
 	private boolean containsJet(Jet jet) {
-		if(jets.length == 0) {
-			return false;
-		}
-		
-		for(int i=0; i < numJets; i++) {
-			if(jets[i].equals(jet))
-				return true;
-		}
-
-		return false;
+		return jets.contains(jet);
 	}
 
-	private void expandArray() {
-		Jet[] newArray = new Jet[jets.length*2];
-		System.arraycopy(jets, 0, newArray, 0, numJets);
-		jets = newArray;
-	}
-	
 	public Jet[] getJets() {
-		Jet[] jetList = new Jet[numJets];
-		System.arraycopy(jets, 0, jetList, 0, numJets);
-		
-		return jetList;
+		return jets.toArray(new Jet[0]);
 	}
 }
